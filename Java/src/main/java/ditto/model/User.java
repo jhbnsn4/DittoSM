3f53abb132f982b14b2fcd4f285c22dd6cd9f7ce
JaeKLee <leejae0118@gmail.com>
2021-10-23 01:16:16 -0700

    Incorporated what we learned

---
+++ Java/src/main/java/ditto/model/User.java
@@ -0,0 +1,14 @@
+package ditto.model;
+
+import java.util.Date;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String userPassword;
+	private String userFName;
+	private String userLName;
+	private Date dob;
+	
+
+}
