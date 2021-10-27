9f004c21f178a03334776fd17905af2241d25795
Ryan Moss <rymoss.9270@gmail.com>
2021-10-26 23:05:05 -0700

    added basic functionality for user profile display/update

--- project3 - Angular/src/app/components/user-profile/user-profile.component.ts
+++ project3 - Angular/src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IImageMap } from 'src/app/model/imagemap';
 import { IUserAccount } from 'src/app/model/useraccount';
 import { AjaxService } from 'src/app/service/ajax.service';
 
@@ -9,17 +10,71 @@ import { AjaxService } from 'src/app/service/ajax.service';
 })
 export class UserProfileComponent implements OnInit {
 
+  private targetUser: IUserAccount | undefined;
+  public editing: boolean = false;
+
   constructor(private ajaxService: AjaxService) { }
 
   ngOnInit(): void {
-  }
 
-  onSearchById(event: Event) {
+
     let response = this.ajaxService.getUserById(1).subscribe(
       data => {
-        console.log(data);
+        this.targetUser = data;
+        //console.log(this.firstName);
       }
     );
-
   }
+
+  /////////// GETTERS & SETTERS
+  get firstName() {
+    return (this.targetUser) ? this.targetUser.firstName : "";
+  }
+  set firstName(firstName: string) {
+    if (this.targetUser)
+      this.targetUser.firstName = firstName;
+  }
+  get lastName() {
+    return (this.targetUser) ? this.targetUser.lastName : "";
+  }
+  set lastName(lastName: string) {
+    if (this.targetUser)
+      this.targetUser.lastName = lastName;
+  }
+  get birthday() {
+    return (this.targetUser) ? this.targetUser.birthday : new Date();
+  }
+  set birthday(birthday: Date) {
+    if (this.targetUser) {
+      this.targetUser.birthday = birthday;
+      // console.log(this.targetUser.birthday.toDateString());
+    }
+  }
+  parseDate(): String {
+    return (this.targetUser) ? new Date(this.targetUser.birthday).toISOString().split('T')[0] : "";
+  }
+  get statusText() {
+    return (this.targetUser) ? this.targetUser.statusText : "";
+  }
+  set statusText(statusText: string) {
+    if (this.targetUser)
+      this.targetUser.statusText = statusText;
+  }
+  // TOOD: Add getter/setter for profile picture
+
+  //////////// OTHER METHODS
+
+  onClickEdit(event: Event) {
+    (document.getElementById("profileFieldset") as HTMLInputElement).disabled = this.editing;
+    this.editing = !this.editing;
+  }
+
+  onClickUpdateProfile() {
+    let response = this.ajaxService.updateUser(this.targetUser as IUserAccount).subscribe(
+      data => {
+        console.log("update data: " + data);
+      }
+    );
+  }
+
 }
--- project3 - Angular/src/app/service/ajax.service.ts
+++ project3 - Angular/src/app/service/ajax.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { IUserAccount } from "../model/useraccount";
@@ -15,4 +15,16 @@ export class AjaxService {
     getUserById(id: number): Observable<IUserAccount> {
         return this.myHttpCli.get<IUserAccount>(`http://localhost:9015/DittoSM/api/users/getUserById?id=${id}`);
     }
+
+    updateUser(user: IUserAccount): Observable<string> {
+        console.log("updating user: " + user.username);
+        const httpPost = {
+          headers: new HttpHeaders({
+            'Content-Type':  'application/json',
+          })
+        };
+    
+        return this.myHttpCli.put<string>('http://localhost:9015/DittoSM/api/users/updateUser', 
+        user, httpPost);
+      }
 }
