2a0236638c19b6d44dace7f123627c3a0331186d
Michael Keefer <mike.keefer@revature.net>
2021-11-02 07:42:29 -0700

    11/2 added Controller class testing

--- project3 - Spring/DittoSM/src/main/java/dittoSM/model/MyCustomMessage.java
+++ project3 - Spring/DittoSM/src/main/java/dittoSM/model/MyCustomMessage.java
@@ -1,5 +1,6 @@
 package dittoSM.model;
 
+import java.util.Objects;
 
 /**
  * MK: added MyCustomMessage class to customize http request messages
@@ -41,5 +42,24 @@ public class MyCustomMessage {
 		return "MessageCustomizedForThisExample [message=" + message + ", otherPossibleInformation="
 				+ otherPossibleInformation + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(message, otherPossibleInformation);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MyCustomMessage other = (MyCustomMessage) obj;
+		return Objects.equals(message, other.message)
+				&& Objects.equals(otherPossibleInformation, other.otherPossibleInformation);
+	}
+	
 	
 }
